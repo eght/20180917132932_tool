8a58b02f35e27663806f30c4f4ee828dcaa9b162
eght <stephniegill@gmail.com>
2021-07-03 10:14:53 +0800

    win10 bat
    win10 startup auto run bat

---
+++ _win10-startup-auto-run.bat
@@ -0,0 +1,18 @@
+@echo off
+echo starting 01. Everything
+start /min "" "E:\Program Files (x86)\Everything\Everything.exe"
+ 
+echo starting  02. Firefox
+start /min "" "E:\Program Files\Mozilla Firefox\firefox.exe" 
+
+:echo starting  03. TeXworks
+:start /min "" "E:\texlive\2020\bin\win32\texworks.exe" 
+ 
+echo starting  04. Emacs
+start /min "" "F:\Portable\Emacs\runemacs - ShortCuts.lnk"
+
+echo starting  05. TeXworks, work, routine,
+start /min "" "F:\git-testing\secret\c\cm\cmw\cmwk\000-000-000\E1A263E4-DA55-4575-B982-F3905E4D55E3.tex"
+
+echo starting  06. Gvim
+start /min "" "F:\Portable\Vim\vim80\gvim.exe"
